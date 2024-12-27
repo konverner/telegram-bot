b904c4866aedd061d318caa6d4401efd02e356c0
konverner <konst.verner@gmail.com>
2024-12-27 22:43:36 +0100

    feat: add google sheets integration

--- src/telegram_bot/api/handlers/apps/__init__.py
+++ src/telegram_bot/api/handlers/apps/__init__.py
@@ -1,10 +1,13 @@
 from telebot import TeleBot
-from telegram_bot.api.handlers.apps import audio, google_drive, llm, menu, resource
+
+from telegram_bot.api.handlers.apps import audio, google_drive, google_sheets, llm, menu, resource, start
 
 
 def register_handlers(bot: TeleBot):
+    start.register_handlers(bot)
     resource.register_handlers(bot)
     audio.register_handlers(bot)
     llm.register_handlers(bot)
     google_drive.register_handlers(bot)
+    google_sheets.register_handlers(bot)
     menu.register_handlers(bot)
--- src/telegram_bot/api/handlers/apps/google_drive.py
+++ src/telegram_bot/api/handlers/apps/google_drive.py
@@ -7,7 +7,9 @@ from omegaconf import OmegaConf
 from telebot import TeleBot, types
 from telebot.states import State, StatesGroup
 from telebot.states.sync.context import StateContext
+
 from telegram_bot.core.google_drive import GoogleDriveService
+from telegram_bot.db.models import User
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
