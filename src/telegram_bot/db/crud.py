7d6822d2560c3f2173ef4700a51ab1567a6a6c0a
Konstantin Verner <konst.verner@gmail.com>
2025-01-18 23:43:05 +0100

    feat: add language switch app

--- src/telegram_bot/db/crud.py
+++ src/telegram_bot/db/crud.py
@@ -25,6 +25,7 @@ def read_user(id: int) -> User:
     db.close()
     return result
 
+
 def read_user_by_username(username: str) -> User:
     """Read user by username"""
     db: Session = get_session()
